83883f4a9ebd03a9f516d762ee457ce5ec648976
samratborade <samborade14@gmail.com>
2024-10-14 08:01:53 +0530

    added single inheritance program

---
+++ src/practice_q/Child.java
@@ -0,0 +1,12 @@
+package practice_q;
+
+public class  Child extends Parent {
+
+	public static void main(String[] args) {
+		
+		Child obj=new Child ();
+		 obj.demo();
+
+	}
+
+}
---
+++ src/practice_q/Parent.java
@@ -0,0 +1,9 @@
+package practice_q;
+
+public class Parent {
+
+	public void demo ()
+	{
+		System.out.println("this method belongs to parent class");
+	}	
+}
